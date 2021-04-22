9823029fb0c3f615332c7e51be6f15aa8101eda3
xpanse-aeron <aeron.xpanse@gmail.com>
2021-04-22 10:13:42 +0800

    First commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,41 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useRouter } from 'next/router'
+import Nav from "../components/Nav"
+import Footer from "../components/Footer"
+import SearchDropdown from "../components/SearchDropdowns"
+import useSWR from "swr";
+import { useUser } from '@auth0/nextjs-auth0';
 
 export default function Home() {
+  const router = useRouter();
+  const { user, error:userError, isLoading } = useUser();
+  const { data: count, error } = useSWR("/api/list_count")
+
   return (
-    <div className={styles.container}>
+    <div className="flex flex-col">
       <Head>
-        <title>Create Next App</title>
+        <title>WorkLocal | Powered by WorkAbroad.ph</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      
+      <Nav />
+      {router.pathname == "/" ?
+      <main className="bg-gray-100 h-58rem flex justify-center items-center">
+        <div className="container m-auto text-center">
+          <h3 className="text-6xl font-bold text-gray-700 mb-3">
+            Find the job you love
+          </h3>
+          <p className="text-2xl text-sm mt-4 text-gray-600 mb-5">Discover over 39,272 jobs in the Philippines</p>
+          <SearchDropdown />
+          <div className="mt-10 text-2xl hidden xl:block lg:block">
+            <b>{count}</b> Popular Jobs &nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;<b>{count}</b> Companies
+          </div>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      :
+      null
+      }
+      <Footer />
     </div>
   )
 }
