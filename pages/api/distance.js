9b2e6c51a53b8da20092388d1055cdd8e103e51e
xpanse-aeron <aeron.xpanse@gmail.com>
2021-06-01 18:19:20 +0800

    disabled /api/distance

--- pages/api/distance.js
+++ pages/api/distance.js
@@ -2,7 +2,7 @@ import fetch from "isomorphic-unfetch";
 
 export default async function Coordinates (req, res) {
 	try {
-		await fetch("https://maps.googleapis.com/maps/api/distancematrix/json?origins="+ req.body.coordinates +"&destinations=14.47584617213881,121.19567387159655&key=AIzaSyDS2KtzKos2ZSc4ALwlxRZQzE_zDHcWRNg", {
+		await fetch("https://maps.googleapis.com/maps/api/distancematrix/json?origins="+ req.body.coordinates +"&destinations=14.47584617213881,121.19567387159655&key=AIzaSyCEXBDZsAuMGx2E8baYRoqghA31OOzzn14", {
 			method: "GET"
 		})
 		.then((r) => r.json())
